29826f588f8c6c097f16808520699a07730616c5
Amr <amrasani8386@gmail.com>
2026-08-28 23:25:42 +0300

    Clean server config

--- config/server.ts
+++ config/server.ts
@@ -1,3 +1,23 @@
+// import type { Core } from '@strapi/strapi';
+
+// const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS')!,
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT'),
+//     },
+//   } as unknown as Core.Config.Server['transfer'],
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
+// export default config;
+
 import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
